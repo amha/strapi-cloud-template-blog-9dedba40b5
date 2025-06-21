f121d0f028da2dff842d7033f1370d30b7219af2
Amha Mogus <amha.mogus@gmail.com>
2025-06-21 12:49:10 -0400

    Explicitly defining admin path and public url in server settings.

--- config/server.js
+++ config/server.js
@@ -1,8 +1,15 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('PUBLIC_URL', 'https://strapi-cloud-template-blog-9dedba40b5.onrender.com'),
   app: {
-    keys: env.array('APP_KEYS', ['someKey1', 'someKey2'])
+    keys: env.array('APP_KEYS', ['someKey1', 'someKey2']),
+  },
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET'),
+    },
+    url: '/admin', 
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
